9dc6d5a9dfd99640f9baf04800c6cce287989bf1
Fakhar Hussain <fakharhussain.179@gmail.com>
2023-04-11 16:01:20 +0500

    React Todo-List Completed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoArray, setTodoArray] = useState([]);
+
+  const AddData = () => {
+    setTodoArray([todo , ...todoArray]);
+    setTodo('');
+    localStorage.setItem("todo", JSON.stringify([todo, ...todoArray]));
+  }
+
+  const RemoveAllData = () => {
+    setTodoArray([]);
+    localStorage.clear();
+    setTodo("")
+  }
+
+  const RemoveItem = (id) => {
+    let updatedList = todoArray.filter((item,index) => {
+      return index !== id
+    })
+    localStorage.setItem("todo", JSON.stringify(updatedList))
+    setTodoArray(updatedList);
+  }
+
+  useEffect( () => {
+    let items = localStorage.getItem("todo")
+    let data = JSON.parse(items);
+    if (data !== null) {
+      console.log(data);
+      setTodoArray(data)
+    }
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <span>welcome to our new project</span>
+        <h2>Todo List</h2>
+      </div>
+
+      <div className="main-content">
+        <input value={todo} onChange={(e) => setTodo(e.currentTarget.value)} type="text" placeholder="Add Todo Item's Here" className="inputField" />
+        <button onClick={AddData} className="addBtn">Add Todo's</button>
+        <button onClick={RemoveAllData} className="deleteAllBtn">Remove All</button>
+        <hr className="longHR" />
+      </div>
+
+
+      {
+        todoArray.map( (item , index) => {
+          return(
+            <div key={index}>
+              <div  className="contents">
+                <p className="todoTxt">{item}</p>
+                <button onClick={() => RemoveItem(index)} className="deleteBtn">Remove Todo</button>
+              </div>
+                <br/>
+                <hr className="shortHR"  />
+            </div>
+          )
+        })
+      }
+
+      {/* {
+        todoArray !== ""
+        ?
+
+        : 
+        null
+      } */}
+
+      <br />
     </div>
   );
 }
